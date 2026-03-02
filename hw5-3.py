06697f00f848a2e5c7b162de798deaafc6ca6510
VladyslavKovalenko <wild.wolf.ua@gmail.com>
2026-03-02 14:41:27 +0200

    hw5-3 is done

---
+++ hw5-3.py
@@ -0,0 +1,13 @@
+import string
+
+text = input("Введіть рядок: ")
+
+for symbol in string.punctuation:
+    text = text.replace(symbol, "")
+
+words = text.split()
+hashtag = "#" + "".join(word.capitalize() for word in words)
+
+hashtag = hashtag[:140]
+
+print(hashtag)
